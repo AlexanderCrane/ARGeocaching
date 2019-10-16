3be9846ff2e3b3725ed0fc714fc543ac8e5b8f2a
AlexanderCrane <InnovateTheWorld@yahoo.com>
2019-10-16 01:24:22 -0500

    testing firebase functionality in new branch

--- ARGeocachingApp/Assets/AlexScripts/TreasureManager.cs
+++ ARGeocachingApp/Assets/AlexScripts/TreasureManager.cs
@@ -39,6 +39,22 @@ public class TreasureManager : Singleton<TreasureManager> {
 
       //toggle visibility of not found treasure chests
 
+      Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+        var dependencyStatus = task.Result;
+        if (dependencyStatus == Firebase.DependencyStatus.Available) {
+          print("Firebase dependencies resolved successfully");
+          // Create and hold a reference to your FirebaseApp,
+          // where app is a Firebase.FirebaseApp property of your application class.
+          //   app = Firebase.FirebaseApp.DefaultInstance;
+
+          // Set a flag here to indicate whether Firebase is ready to use by your app.
+        } else {
+          UnityEngine.Debug.LogError(System.String.Format(
+            "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+          // Firebase Unity SDK is not safe to use here.
+        }
+      });
+
   }
 
 	public IEnumerator PlaceTreasure()
